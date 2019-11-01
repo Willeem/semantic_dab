b8cd05feae7df07191fe98d0af02078e1bed9380
Willeem <willem.datema@hotmail.com>
2019-11-01 15:45:14 +0100

    json moet lists hebben

--- compare_properties.py
+++ compare_properties.py
@@ -12,11 +12,11 @@ for letter in "abcdefghijklmnopqrstuvwxyz":
     similar_keys = {k for k in g_dict if k in h_dict}
 
     types = [string.split()[1] for key in similar_keys for string in h_dict[key]]
-    output_dict = defaultdict(set)
+    output_dict = defaultdict(list)
     for key in similar_keys:
         for string in g_dict[key]:
             if string.split()[1] not in types and "@en" not in string:
-                output_dict[string.split()[0]].add(string)
+                output_dict[string.split()[0]].append(string)
     with open("new_triples/new_property_nl_%s.json" %letter, 'w') as js:
         json.dump(output_dict,js)
     print("%s done" %letter)
